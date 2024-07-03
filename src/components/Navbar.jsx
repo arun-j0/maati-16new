3f8e52a428381d1cd768860056c79e7f9c578cb4
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2024-07-03 11:42:23 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { FaHome, FaInfoCircle, FaBriefcase, FaImages, FaProjectDiagram, FaBlog, FaPhone, FaDonate, FaBars, FaTimes } from 'react-icons/fa';
+import { FaHome, FaInfoCircle, FaBriefcase, FaImages, FaProjectDiagram, FaBlog, FaDonate, FaBars, FaTimes } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import logo from '../assets/logo.png';
-import PersonalDetails from './PersonalDetails';
 
 const Navbar = () => {
   const location = useLocation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeDropdown, setActiveDropdown] = useState(null);
-  const [showPersonalDetails, setShowPersonalDetails] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-    setActiveDropdown(null); // Close any open dropdowns when menu is toggled
+    setActiveDropdown(null);
   };
 
   const closeMenu = () => {
@@ -23,18 +21,18 @@ const Navbar = () => {
 
   const toggleDropdown = (dropdownName) => {
     if (activeDropdown === dropdownName) {
-      setActiveDropdown(null); // Close dropdown if already open
+      setActiveDropdown(null);
     } else {
-      setActiveDropdown(dropdownName); // Open the clicked dropdown
+      setActiveDropdown(dropdownName);
     }
   };
 
   const handleMouseEnter = (dropdownName) => {
-    setActiveDropdown(dropdownName); // Open dropdown on hover
+    setActiveDropdown(dropdownName);
   };
 
   const handleMouseLeave = () => {
-    setActiveDropdown(null); // Close dropdown on mouse leave
+    setActiveDropdown(null);
   };
 
   const isActive = (path) => location.pathname === path;
@@ -43,8 +41,6 @@ const Navbar = () => {
     hidden: { opacity: 0, y: -10 },
     visible: { opacity: 1, y: 0 },
   };
-
-
 
   return (
     <header className="z-10 bg-gray-100 shadow">
@@ -203,203 +199,179 @@ const Navbar = () => {
                 className={`flex items-center py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/contact-us') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
                 onClick={closeMenu}
               >
-                <FaPhone className="mr-2" />Contact Us
+                <FaEnvelope className="mr-2" />Contact Us
               </Link>
             </li> */}
             <li>
               <Link
-                to="/donate-us"
-                className={`flex items-center py-2 px-4 rounded-lg transition-colors duration-300 bg-yellow-500 text-white hover:bg-amber-900`}
+                to="/donate"
+                className={`flex items-center py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/donate') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
                 onClick={closeMenu}
               >
-                <FaDonate className="mr-2" />Donate Us
+                <FaDonate className="mr-2" />Donate
               </Link>
             </li>
           </ul>
         </div>
 
-        <div className="absolute md:hidden right-4 top-9 ">
-          {!isMenuOpen ? (
-            <button
-              onClick={toggleMenu}
-              className="block text-gray-800 hover:text-amber-800 focus:outline-none"
-            >
-              <FaBars className="w-6 h-6" />
-            </button>
-          ) : (
-            <button
-              onClick={closeMenu}
-              className="block text-gray-800 hover:text-amber-800 focus:outline-none"
-            >
-              <FaTimes className="w-6 h-6" />
-            </button>
-          )}
+        <div className="md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="p-2 text-gray-800 rounded-lg focus:outline-none focus:shadow-outline"
+          >
+            {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+          </button>
         </div>
-
-        <ul className={`md:hidden w-[90%] rounded-lg mx-auto ${isMenuOpen ? 'flex-col bg-gray-100 absolute inset-x-0 top-[64px] z-50' : 'hidden'}`}>
-          <li>
-            <Link
-              to="/"
-              className={`flex items-center py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
-              onClick={closeMenu}
-            >
-              <FaHome className="mr-2" />Home
-            </Link>
-          </li>
-
-          <li className="relative group">
-            <span
-              className={`flex items-center cursor-pointer py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/about-us') ? 'bg-amber-800 text-white' : 'text-gray-800 group-hover:text-white group-hover:bg-amber-800'}`}
-              onClick={() => toggleDropdown('aboutUs')}
-              onMouseEnter={() => handleMouseEnter('aboutUs')}
-              onMouseLeave={handleMouseLeave}
-            >
-              <FaInfoCircle className="mr-2" />About Us
-            </span>
-            <motion.ul
-              initial="hidden"
-              animate={activeDropdown === 'aboutUs' ? "visible" : "hidden"}
-              variants={dropdownVariants}
-              className={`absolute ${activeDropdown === 'aboutUs' ? 'block' : 'hidden'} mt-[1px] bg-white rounded shadow-lg w-56 transition-all duration-300 ease-in-out z-20`}
-              onMouseEnter={() => handleMouseEnter('aboutUs')}
-              onMouseLeave={handleMouseLeave}
-            >
-              <li>
-                <Link
-                  to="/personal-details"
-                  className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
-                  onClick={closeMenu}
-                >
-                  Personal Details
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/core-members"
-                  className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
-                  onClick={closeMenu}
-                >
-                  Core Members
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/motto-objective"
-                  className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
-                  onClick={closeMenu}
-                >
-                  Our Motto and Objective
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/faqs-policies"
-                  className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
-                  onClick={closeMenu}
-                >
-                  FAQs and Policies
-                </Link>
-              </li>
-            </motion.ul>
-          </li>
-
-          <li className="relative group">
-            <span
-              className={`flex items-center cursor-pointer py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/careers') ? 'bg-amber-800 text-white' : 'text-gray-800 group-hover:text-white group-hover:bg-amber-800'}`}
-              onClick={() => toggleDropdown('careers')}
-              onMouseEnter={() => handleMouseEnter('careers')}
-              onMouseLeave={handleMouseLeave}
-            >
-              <FaBriefcase className="mr-2" />Careers
-            </span>
-            <motion.ul
-              initial="hidden"
-              animate={activeDropdown === 'careers' ? "visible" : "hidden"}
-              variants={dropdownVariants}
-              className={`absolute ${activeDropdown === 'careers' ? 'block' : 'hidden'} mt-[1px] bg-white shadow-lg w-[270px] rounded-lg transition-all duration-300 ease-in-out z-20`}
-              onMouseEnter={() => handleMouseEnter('careers')}
-              onMouseLeave={handleMouseLeave}
-            >
-              <li>
-                <Link
-                  to="/membership-volunteership"
-                  className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
-                  onClick={closeMenu}
-                >
-                  Membership/Volunteership Form
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/careers"
-                  className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
-                  onClick={closeMenu}
-                >
-                  Careers
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/internships"
-                  className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
-                  onClick={closeMenu}
-                >
-                  Internships
-                </Link>
-              </li>
-            </motion.ul>
-          </li>
-
-          <li>
-            <Link
-              to="/gallery"
-              className={`flex items-center py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/gallery') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
-              onClick={closeMenu}
-            >
-              <FaImages className="mr-2" />Gallery
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/our-works"
-              className={`flex items-center py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/our-works') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
-              onClick={closeMenu}
-            >
-              <FaProjectDiagram className="mr-2" />Our Works
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/blogs"
-              className={`flex items-center py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/blogs') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
-              onClick={closeMenu}
-            >
-              <FaBlog className="mr-2" />Blogs
-            </Link>
-          </li>
-          {/* <li>
-            <Link
-              to="/contact-us"
-              className={`flex items-center py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/contact-us') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
-              onClick={closeMenu}
-            >
-              <FaPhone className="mr-2" />Contact Us
-            </Link>
-          </li> */}
-          <li>
-            <Link
-              to="/donate-us"
-              className={`flex items-center py-2 px-4 rounded-lg transition-colors duration-300 bg-yellow-500 text-white hover:bg-amber-900`}
-              onClick={closeMenu}
-            >
-              <FaDonate className="mr-2" />Donate Us
-            </Link>
-          </li>
-        </ul>
       </nav>
 
+      {isMenuOpen && (
+        <div className="text-center md:hidden">
+          <ul className="py-2 space-y-2">
+            <li>
+              <Link
+                to="/"
+                className={`block py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
+                onClick={closeMenu}
+              >
+                <FaHome className="inline-block mr-2" />Home
+              </Link>
+            </li>
+            <li>
+              <span
+                className={`flex items-center justify-center cursor-pointer py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/about-us') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
+                onClick={() => toggleDropdown('aboutUs')}
+              >
+                <FaInfoCircle className="mr-2" />About Us
+              </span>
+              {activeDropdown === 'aboutUs' && (
+                <ul className="space-y-2">
+                  <li>
+                    <Link
+                      to="/personaldetails"
+                      className="block w-full px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
+                      onClick={closeMenu}
+                    >
+                      Personal Details
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/core-members"
+                      className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
+                      onClick={closeMenu}
+                    >
+                      Core Members
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/motto-objective"
+                      className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
+                      onClick={closeMenu}
+                    >
+                      Our Motto and Objective
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/faqs"
+                      className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
+                      onClick={closeMenu}
+                    >
+                      FAQs and Policies
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li>
+              <span
+                className={`flex items-center justify-center cursor-pointer py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/careers') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
+                onClick={() => toggleDropdown('careers')}
+              >
+                <FaBriefcase className="mr-2" />Careers
+              </span>
+              {activeDropdown === 'careers' && (
+                <ul className="space-y-2">
+                  <li>
+                    <Link
+                      to="/membership-volunteership"
+                      className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
+                      onClick={closeMenu}
+                    >
+                      Membership/Volunteership Form
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/careers"
+                      className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
+                      onClick={closeMenu}
+                    >
+                      Careers
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/internships"
+                      className="block px-4 py-2 text-gray-800 transition-colors duration-300 rounded-lg hover:text-white hover:bg-amber-800"
+                      onClick={closeMenu}
+                    >
+                      Internships
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li>
+              <Link
+                to="/gallery"
+                className={`block py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/gallery') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
+                onClick={closeMenu}
+              >
+                <FaImages className="inline-block mr-2" />Gallery
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/our-works"
+                className={`block py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/our-works') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
+                onClick={closeMenu}
+              >
+                <FaProjectDiagram className="inline-block mr-2" />Our Works
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/blogs"
+                className={`block py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/blogs') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
+                onClick={closeMenu}
+              >
+                <FaBlog className="inline-block mr-2" />Blogs
+              </Link>
+            </li>
+            {/* <li>
+              <Link
+                to="/contact-us"
+                className={`block py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/contact-us') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
+                onClick={closeMenu}
+              >
+                <FaEnvelope className="inline-block mr-2" />Contact Us
+              </Link>
+            </li> */}
+            <li>
+              <Link
+                to="/donate"
+                className={`block py-2 px-4 rounded-lg transition-colors duration-300 ${isActive('/donate') ? 'bg-amber-800 text-white' : 'text-gray-800 hover:text-white hover:bg-amber-800'}`}
+                onClick={closeMenu}
+              >
+                <FaDonate className="inline-block mr-2" />Donate
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
-    
   );
 };
 
