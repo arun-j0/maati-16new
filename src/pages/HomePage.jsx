d936cec76554ad79546407b44af47d1889cf6977
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2024-07-03 12:04:57 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import HeroSection from '../components/Hero'
-import IntroSection from '../components/Intro'
-import SponsorSection from '../components/Sponsor'
-import ConnectWithUs from '../components/Connect'
-import Footer from '../components/Footer'
+// src/pages/HomePage.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/Hero';
+import IntroSection from '../components/Intro';
+import SponsorSection from '../components/Sponsor';
+import ConnectWithUs from '../components/Connect';
+import Footer from '../components/Footer';
 
 function HomePage() {
-  return (
-    <>
-        <Navbar/>
-        <HeroSection/>
-        <IntroSection/>
-        <SponsorSection/>
-        <ConnectWithUs/>
-        <Footer/>
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <HeroSection />
+            <IntroSection />
+            <SponsorSection />
+            <ConnectWithUs />
+            <Footer />
+        </>
+    );
 }
 
-export default HomePage
+export default HomePage;
