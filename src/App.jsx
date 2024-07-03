2f75831090a981d5ce697e9345789bc6cc784a96
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2024-07-03 12:00:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GalleryPage from './pages/GalleryPage';
@@ -6,21 +7,21 @@ import WorkPage from './pages/WorkPage';
 import PersonalDetailsPage from './pages/PersonalDetailsPage';
 import FaqsPage from './pages/FaqsPage';
 import MembershipForm from './components/MembershipForm';
+import Donation from './components/Donation';
 
 function App() {
     return (
         <Router>
-            <>
-              <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/gallery" element={<GalleryPage />} />
-                    <Route path="/blogs" element={<BlogPage />} />
-                    <Route path="/our-works" element={<WorkPage />} />
-                    <Route path="/personaldetails" element={<PersonalDetailsPage />} />
-                    <Route path="/faqs" element={<FaqsPage />} />
-                    <Route path="/membership" element={<MembershipForm />} />
-              </Routes>
-            </>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/gallery" element={<GalleryPage />} />
+                <Route path="/blogs" element={<BlogPage />} />
+                <Route path="/our-works" element={<WorkPage />} />
+                <Route path="/personaldetails" element={<PersonalDetailsPage />} />
+                <Route path="/faqs" element={<FaqsPage />} />
+                <Route path="/membership" element={<MembershipForm />} />
+                <Route path="/donation" element={<Donation />} />
+            </Routes>
         </Router>
     );
 }
