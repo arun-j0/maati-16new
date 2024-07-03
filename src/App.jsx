65139a076cec666baaf0952cd8cd394deed3c6c7
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2024-07-03 12:05:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // src/App.jsx
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GalleryPage from './pages/GalleryPage';
