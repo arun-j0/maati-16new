ed5d2b291324f7f356032c0856352077d2335db5
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2024-07-16 21:51:10 +0530

    Update PersonalDetails.jsx

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'; 
 import { FaPhone, FaMapMarkerAlt, FaInstagram, FaLinkedin, FaUser } from 'react-icons/fa';
 import HemantDubeyImage from '../assets/Hemant_Dubey.jpg'; // Import the image
 
