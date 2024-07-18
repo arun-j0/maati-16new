466e9c092f8a652ba1e0f1ff8929c6506f570a0c
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2024-07-18 20:15:50 +0530

    Update BlogDetailPage.jsx

--- src/pages/BlogDetailPage.jsx
+++ src/pages/BlogDetailPage.jsx
@@ -6,7 +6,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick-theme.css'; 
 
 const placeholderImage = "https://designshack.net/wp-content/uploads/placeholder-image.png";
 
