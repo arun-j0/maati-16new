7b8217761fb0118a1382650a71ff89ea741a1be5
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2024-07-26 14:52:16 +0530

    Update Sponsor.jsx

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -37,10 +37,9 @@ const SponsorSection = () => {
     {
       title: "Provide Meals for a Week",
       description: `Sponsor a snack for 100 children:
-Fruit: ~INR 1000 
-Samosa: ~INR 1000 
-Juice/Flavored Milk: ~INR 1000 ,
-
+Fruit: ~INR 600
+Samosa: ~INR 1500
+Juice/Flavored Milk: ~INR 1900`,
       button: "Donate for Meals",
     },
     {
