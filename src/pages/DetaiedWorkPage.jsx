abdc2b3543268de3b156c2a7f30787ab0817363e
arun-j0 <arun2310kumar2002@gmail.com>
2024-07-30 01:08:14 +0530

    detailed work page completed

--- src/pages/DetaiedWorkPage.jsx
+++ src/pages/DetaiedWorkPage.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -6,26 +6,123 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const placeholderImage = "https://designshack.net/wp-content/uploads/placeholder-image.png";
+import id1_img1 from "../assets/works/food-drive-covid/image1.png";
+import id1_img2 from "../assets/works/food-drive-covid/image2.png";
+import id1_img3 from "../assets/works/food-drive-covid/image3.png";
+
+import id2_img1 from "../assets/works/mask-donation-drive/image1.png";
+import id2_img2 from "../assets/works/mask-donation-drive/image2.png";
+import id2_img3 from "../assets/works/mask-donation-drive/image3.png";
+
+import id3_img1 from "../assets/works/project-education-mission/image1.png";
+import id3_img2 from "../assets/works/project-education-mission/image2.png";
+import id3_img3 from "../assets/works/project-education-mission/image3.png";
+import id3_img4 from "../assets/works/project-education-mission/image4.png";
+import id3_img5 from "../assets/works/project-education-mission/image5.png";
+
+import id4_img1 from "../assets/works/project-smile/image1.png";
+import id4_img2 from "../assets/works/project-smile/image2.png";
+import id4_img3 from "../assets/works/project-smile/image3.png";
+import id4_img4 from "../assets/works/project-smile/image4.png";
+
+import id5_img1 from "../assets/works/blanket-donation-drive1/image1.png";
+import id5_img2 from "../assets/works/blanket-donation-drive1/image2.png";
+import id5_img3 from "../assets/works/blanket-donation-drive1/image3.png";
+
+import id6_img1 from "../assets/works/blanket-donation-drive2/image1.png";
+import id6_img2 from "../assets/works/blanket-donation-drive2/image2.png";
+import id6_img3 from "../assets/works/blanket-donation-drive2/image3.png";
+import id6_img4 from "../assets/works/blanket-donation-drive2/image4.png";
+
+import id7_img1 from "../assets/works/children's-day-celebration/image1.png";
+import id7_img2 from "../assets/works/children's-day-celebration/image2.png";
+import id7_img3 from "../assets/works/children's-day-celebration/image3.png";
+import id7_img4 from "../assets/works/children's-day-celebration/image4.png";
+
+import id8_img1 from "../assets/works/stray-dogs/image1.png";
+import id8_img2 from "../assets/works/stray-dogs/image2.png";
+import id8_img3 from "../assets/works/stray-dogs/image3.png";
+import id8_img4 from "../assets/works/stray-dogs/image4.png";
+import id8_img5 from "../assets/works/stray-dogs/image5.png";
+
+import id9_img1 from "../assets/works/food-donation/image1.png";
+import id9_img2 from "../assets/works/food-donation/image2.png";
+import id9_img3 from "../assets/works/food-donation/image3.png";
+import id9_img4 from "../assets/works/food-donation/image4.png";
 
 const projects = [
   {
     id: 1,
-    title: "Fundraiser Event",
-    description: "Content of the project..Content of the project..Content of the project..Content of the project..Content of the project..",
-    detailedDescription: "Detailed description of the project...",
+    title: "Food Donation Drive (COVID-19)",
+    description: "Many families have become financially weak due to the corona pandemic. Due to the collapse of businesses, the livelihoods of all the families have been snatched away. People earning money from daily wages do not have enough ration to eat.",
+    detailedDescription: "Our team organized a Food Donation Drive to help families affected by the COVID-19 pandemic. We distributed essential food items like rice, lentils, oil, and vegetables to support those who lost their income sources during the lockdown.",
     date: "2024-05-10",
-    images: [placeholderImage, placeholderImage, placeholderImage]
+    images: [id1_img1, id1_img2, id1_img3]
   },
   {
     id: 2,
-    title: "Unity in Giving: Community Donation Drive",
-    description: "Join us for our 'Unity in Giving' donation drive, where we're coming together as a community to provide essential items and support to those in need.",
-    detailedDescription: "In response to the ongoing need within our community, we are organizing a special event titled 'Unity in Giving: Community Donation Drive.' This event aims to bring together individuals, businesses, and organizations to support those in need and foster a sense of solidarity and generosity within our community. The donation drive will encompass various initiatives, including collecting essential items such as non-perishable food, clothing, hygiene products, and school supplies. Additionally, we will be accepting monetary contributions to provide assistance to local shelters, food banks, and families facing financial hardship. Our goal is not only to provide immediate relief but also to address the long-term needs of our community. Through partnerships with local charities and social service organizations, we will ensure that donations are distributed equitably and effectively to those who need them most. This event will not only benefit individuals and families facing adversity but also serve as a platform for our community to come together, demonstrate compassion, and make a tangible difference in the lives of others. Together, we can create positive change and build a stronger, more supportive community for everyone. Join us in making a meaningful impact through the power of giving!",
+    title: "Mask Donation Drive",
+    description: "Amid the global pandemic, our team decided to distribute masks to poor people who can't afford them.",
+    detailedDescription: "Along with masks, we also distributed biscuits, cakes to poor kids, and clothes to people living in slums near Moin Ul Haq Stadium, Patna, Bihar. This initiative aimed to protect vulnerable populations from COVID-19 and provide some relief in these challenging times.",
     date: "2024-05-09",
-    images: [placeholderImage]
+    images: [id2_img1, id2_img2, id2_img3]
   },
-  // Add other projects with dates
+  {
+    id: 3,
+    title: "Project Education Mission",
+    description: "Our team visited Safeena Baal Grih (an orphanage) to provide educational materials and essentials to orphan kids.",
+    detailedDescription: "We distributed books (class 1st to class 7th), notebooks, toothpaste, shampoo, soap, indoor games, clothes, and ration kits. We spent half of our day with them, playing games and engaging in various activities, making it a memorable experience for the children.",
+    date: "2024-06-15",
+    images: [id3_img1, id3_img2, id3_img3, id3_img4, id3_img5]
+  },
+  {
+    id: 4,
+    title: "Project Smile",
+    description: "Our team, along with Khitij NGO, distributed study materials and learning kits to 150 children of a slum.",
+    detailedDescription: "Materials given included pencils, erasers, sharpeners, rulers, copies, drawing books, color pencils, and books. The initiative aimed to support the education of underprivileged children and encourage learning.",
+    date: "2024-07-01",
+    images: [id4_img1, id4_img2, id4_img3, id4_img4]
+  },
+  {
+    id: 5,
+    title: "Blanket Donation Drive 1",
+    description: "Providing warmth to the needy during the winter season.",
+    detailedDescription: "Our first blanket donation drive was successfully conducted by our group among the poor and needy people around prime locations in Patna, Bihar. The event aimed to protect the vulnerable population from the harsh winter cold.",
+    date: "2023-12-15",
+    images: [id5_img1, id5_img2, id5_img3]
+  },
+  {
+    id: 6,
+    title: "Blanket Donation Drive 2",
+    description: "Continuing our efforts to provide warmth to those in need.",
+    detailedDescription: "Our second blanket donation drive focused on reaching out to more needy individuals in Patna, ensuring that as many people as possible could stay warm during the cold months.",
+    date: "2024-01-10",
+    images: [id6_img1, id6_img2, id6_img3, id6_img4]
+  },
+  {
+    id: 7,
+    title: "Children's Day Celebration",
+    description: "Celebrating Children's Day with underprivileged kids.",
+    detailedDescription: "Our team, along with Kshitij NGO, celebrated Children's Day by distributing biscuits, samosas, chocolates, sweets, and other eatables to underprivileged kids. We also provided indoor games kits like Carrom and Ludo, making the day special and memorable for the children.",
+    date: "2024-11-14",
+    images: [id7_img1, id7_img2, id7_img3, id7_img4]
+  },
+  {
+    id: 8,
+    title: "Food for Stray Dogs",
+    description: "Providing food and care for stray dogs.",
+    detailedDescription: "We initiated a program to feed stray dogs, ensuring that these animals have access to food and care. Our team regularly feeds stray dogs in various areas, promoting kindness towards animals.",
+    date: "Ongoing",
+    images: [id8_img1, id8_img2, id8_img3, id8_img4, id8_img5]
+  },
+  {
+    id: 9,
+    title: "Food Donation for Orphans",
+    description: "Feeding orphan kids on special occasions.",
+    detailedDescription: "We provide food to orphan kids during special occasions, ensuring they receive nutritious meals and feel loved and cared for. This initiative helps in bringing smiles to their faces and making the occasions special for them.",
+    date: "Ongoing",
+    images: [id9_img1, id9_img2, id9_img3, id9_img4]
+  }
 ];
 
 const DetailedProjectPage = () => {
@@ -45,14 +142,15 @@ const DetailedProjectPage = () => {
     autoplay: true,
     autoplaySpeed: 2000
   };
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <>
-      
       <Navbar />
-      <section className="py-12 bg-[#280101] border-b-4 border-[#fee57e] border-8">
+      <section className="py-12 bg-gray-100 border-b-4 border-[#fee57e] border-8">
         <div className="container mx-auto">
           <div className="p-6 mx-auto bg-white rounded-lg shadow-md w-[95%] lg:w-[80%]">
             <h1 className="text-3xl md:text-5xl font-bold text-[#280101] mb-8">{project.title}</h1>
@@ -61,18 +159,19 @@ const DetailedProjectPage = () => {
                 <img
                   src={project.images[0]}
                   alt={project.title}
-                  className="object-cover w-full lg:w-[80%] h-auto rounded-lg" // Set height to auto to maintain aspect ratio
+                  className="object-cover w-full lg:w-[80%] h-auto rounded-lg"
                 />
               </div>
             ) : (
-              <div className="flex justify-center mb-4"> {/* Centering slider horizontally */}
-                <Slider {...settings} className='w-full rounded-lg lg:max-w-3xl'> {/* Set maximum width for slider */}
+              <div className="flex justify-center mb-4">
+                <Slider {...settings} className="w-full rounded-lg lg:w-3xl">
                   {project.images.map((image, index) => (
                     <div key={index} className="flex justify-center">
                       <img
                         src={image}
                         alt={`Image ${index + 1}`}
-                        className="object-cover w-full h-auto rounded-lg" // Set height to auto to maintain aspect ratio
+                        className="object-cover w-full h-[300px] md:h-[600px] rounded-lg"
+                        style={{ objectFit: 'cover' }}
                       />
                     </div>
                   ))}
@@ -82,11 +181,10 @@ const DetailedProjectPage = () => {
             <p className="mb-4 text-xl font-bold text-gray-600">{project.description}</p>
             <p className="mb-4 text-lg text-gray-600">{project.detailedDescription}</p>
             <div className="flex flex-col items-center justify-between text-sm text-gray-500 md:flex-row">
-              
               <span>{project.date}</span>
             </div>
             <div className="flex justify-center mt-8">
-              <Link to="/works" className="bg-[#fee57e] text-[#280101] py-2 px-4 rounded-md font-semibold hover:bg-yellow-600 transition duration-300">
+              <Link to="/our-works" className="bg-[#fee57e] text-[#280101] py-2 px-4 rounded-md font-semibold hover:bg-yellow-600 transition duration-300">
                 Go Back
               </Link>
             </div>
--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -1,28 +1,85 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { FiSearch } from 'react-icons/fi';
 import { IoMdClose } from 'react-icons/io';
-const placeholderImage = "https://designshack.net/wp-content/uploads/placeholder-image.png";
+import id1 from "../assets/works/food-drive-covid/image1.png";
+import id2 from "../assets/works/mask-donation-drive/image1.png";
+import id3 from "../assets/works/project-education-mission/image1.png";
+import id4 from "../assets/works/project-smile/image1.png";
+import id5 from "../assets/works/blanket-donation-drive1/image1.png";
+import id6 from "../assets/works/blanket-donation-drive2/image1.png";
+import id7 from "../assets/works/children's-day-celebration/image1.png";
+import id8 from "../assets/works/stray-dogs/image1.png";
+import id9 from "../assets/works/food-donation/image1.png";
 
 const projects = [
-  { id:1,
-    title: "Fundraiser Event",
-    description: "Content of the project..Content of the project..Content of the project..Content of the project..Content of the project..",
+  {
+    id: 1,
+    title: "Food Donation Drive (COVID-19)",
+    description: "Many families have become financially weak due to the corona pandemic. Due to the collapse of businesses, the livelihoods of all the families have been snatched away. People earning money from daily wages do not have enough ration to eat.",
     date: "2024-05-10",
-    image: placeholderImage
+    image: id1
   },
-  { id:2,
-    title: "Donation",
-    description: "Content of the project..Content of the project..Content of the project..Content of the project..Content of the project..",
+  {
+    id: 2,
+    title: "Mask Donation Drive",
+    description: "Amid the global pandemic, our team decided to distribute masks to poor people who can't afford them.",
     date: "2024-05-09",
-    image: placeholderImage
+    image: id2
   },
-
+  {
+    id: 3,
+    title: "Project Education Mission",
+    description: "Our team visited Safeena Baal Grih (an orphanage) to provide educational materials and essentials to orphan kids.",
+    date: "2024-06-15",
+    image: id3
+  },
+  {
+    id: 4,
+    title: "Project Smile",
+    description: "Our team, along with Khitij NGO, distributed study materials and learning kits to 150 children of a slum.",
+    date: "2024-07-01",
+    image: id4
+  },
+  {
+    id: 5,
+    title: "Blanket Donation Drive 1",
+    description: "Providing warmth to the needy during the winter season.",
+    date: "2023-12-15",
+    image: id5
+  },
+  {
+    id: 6,
+    title: "Blanket Donation Drive 2",
+    description: "Continuing our efforts to provide warmth to those in need.",
+    date: "2024-01-10",
+    image: id6
+  },
+  {
+    id: 7,
+    title: "Children's Day Celebration",
+    description: "Celebrating Children's Day with underprivileged kids.",
+    date: "2024-11-14",
+    image: id7
+  },
+  {
+    id: 8,
+    title: "Food for Stray Dogs",
+    description: "Providing food and care for stray dogs.",
+    date: "Ongoing",
+    image: id8
+  },
+  {
+    id: 9,
+    title: "Food Donation for Orphans",
+    description: "Feeding orphan kids on special occasions.",
+    date: "Ongoing",
+    image: id9
+  }
 ];
-
 
 const WorkPage = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -47,88 +104,81 @@ const WorkPage = () => {
   );
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to top when component mounts
   }, []);
 
   return (
     <>
-      
       <Navbar />
-      <section className="py-12 bg-gray-100 border-b-4 border-[#fee57e] border-8 ">
+      <section className="py-12 bg-gray-100 border-b-4 border-[#fee57e] border-8">
         <div className="container mx-auto">
-        <motion.h2
-        className="pb-5 mb-4 text-4xl font-bold text-center "
-        initial={{ y: -50, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-       <span className='text-4xl font-bold text-[#280101] underline-travel md:text-6xl'> Our Works</span>
-      </motion.h2>
+          <motion.h2
+            className="pb-5 mb-4 text-4xl font-bold text-center"
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <span className='text-4xl font-bold text-[#280101] underline-travel md:text-6xl'>Our Works</span>
+          </motion.h2>
 
           <div className="relative mb-8">
-  <input
-    type="text"
-    value={searchQuery}
-    onChange={(e) => setSearchQuery(e.target.value)}
-    placeholder="Search by project name or date (YYYY-MM-DD)"
-    className="w-full py-3 pl-10 pr-12 text-gray-900 border-2 rounded-full border-amber-800 focus:outline-none focus:border-yellow-600"
-  />
-  <FiSearch className="absolute text-gray-500 transform -translate-y-1/2 top-1/2 left-4" />
-  {searchQuery && (
-    <button
-      onClick={() => setSearchQuery('')}
-      className="absolute text-gray-500 transform -translate-y-1/2 top-1/2 right-4"
-    >
-      <IoMdClose />
-    </button>
-  )}
-</div>
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Search by project name or date (YYYY-MM-DD)"
+              className="w-full py-3 pl-10 pr-12 text-gray-900 border-2 rounded-full border-amber-800 focus:outline-none focus:border-yellow-600"
+            />
+            <FiSearch className="absolute text-gray-500 transform -translate-y-1/2 top-1/2 left-4" />
+            {searchQuery && (
+              <button
+                onClick={() => setSearchQuery('')}
+                className="absolute text-gray-500 transform -translate-y-1/2 top-1/2 right-4"
+              >
+                <IoMdClose />
+              </button>
+            )}
+          </div>
 
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
             {currentProjects.map((project) => (
               <motion.div
                 key={project.id}
-                className="relative overflow-hidden w-[90%] mx-auto"
-                whileHover={{ scale: 1.05 }}
+                className="flex flex-col overflow-hidden bg-white rounded-lg shadow-lg"
+                initial={{ opacity: 0, scale: 0.95 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.3 }}
               >
-               <Link to={`/our-works/${project.id}`}>
-                  <img
-                    src={project.image}
-                    alt={project.title}
-                    className="w-full h-auto rounded-md shadow-md"
-                  />
-                  <motion.div
-                    className="absolute inset-0 flex flex-col items-center rounded-md justify-center text-center bg-[rgba(0,0,0,0.7)] opacity-0 transition-opacity duration-300"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    whileHover={{ opacity: 0 }}
-                  >
-                    <h3 className="mb-2 text-2xl font-bold text-[#fee57e] md:text-4xl">{project.title}</h3>
-                    <p className="max-w-md text-lg text-white md:text-xl">{project.description}</p>
-                    <span className="text-white">Click To See More Details</span>
-                  </motion.div>
-                </Link>
+                <img src={project.image} alt={project.title} className="object-cover w-full h-48" />
+                <div className="flex flex-col flex-grow p-6">
+                  <h3 className="text-xl font-bold text-[#280101] mb-4">{project.title}</h3>
+                  <p className="flex-grow text-base text-gray-700">{project.description}</p>
+                  <Link to={`/our-works/${project.id}`} className="px-4 py-2 mt-4 font-medium text-center text-white transition-colors duration-300 rounded-md bg-amber-500 hover:bg-amber-600">
+                    Click To See More Details
+                  </Link>
+                </div>
               </motion.div>
             ))}
           </div>
 
           {sortedProjects.length > projectsPerPage && (
-  <div className="flex justify-center mt-8">
-    {[...Array(pageCount)].map((_, index) => (
-      <button
-        key={index}
-        onClick={() => {
-          paginate(index + 1);
-          window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to the top
-        }}
-        className={`mx-1 px-4 py-2 bg-[#fee57e] text-[#280101] rounded-md hover:bg-yellow-600 transition duration-300 ${currentPage === index + 1 ? 'font-bold' : ''}`}
-      >
-        {index + 1}
-      </button>
-    ))}
-  </div>
-)}
+            <div className="flex justify-center mt-8">
+              {[...Array(pageCount)].map((_, index) => (
+                <button
+                  key={index}
+                  onClick={() => {
+                    paginate(index + 1);
+                    window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to the top
+                  }}
+                  className={`mx-1 px-4 py-2 bg-[#fee57e] text-[#280101] rounded-md hover:bg-yellow-600 transition duration-300 ${currentPage === index + 1 ? 'font-bold' : ''}`}
+                >
+                  {index + 1}
+                </button>
+              ))}
+            </div>
+          )}
         </div>
       </section>
       <Footer />
